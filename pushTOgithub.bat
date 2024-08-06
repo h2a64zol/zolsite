2a3be20965b943ee5b2597e2eb49c49cb1875437
unknown <C:\Program Files (x86)\The Bat! Professional 5.4 Portable by punsh\Data\Mail>
2024-08-06 13:53:16 +0300

    Change JSON

---
+++ pushTOgithub.bat
@@ -0,0 +1,12 @@
+git status
+rem pause
+git rm *.js *.css *.html *.txt *.ico
+rem pause
+git add .
+rem pause
+git commit -m "Change JSON"
+rem pause
+git pull
+rem pause
+git push -u origin main
+rem pause
